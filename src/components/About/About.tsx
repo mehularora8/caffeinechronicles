8abc177a0a45c0bf559e39e3b068f35ebcc992fc
mehularora8 <aroramehul8@gmail.com>
2024-12-31 17:19:14 -0800

    update def of chain

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,7 +5,7 @@ export function About() {
         
         <div className="prose prose-lg text-md lg:text-lg md:text-md text-gray-800 text-left">
             <p className=" leading-relaxed mb-6">
-                This is a repository of all the local coffee places I’ve been to (<span className="italic">def:</span> any place with &lt; 5 locations at the time of visiting). This list doesn’t include chains. 
+                This is a repository of all the local coffee places I’ve been to (<span className="italic">def:</span> any place with &lt;= 5 locations at the time of visiting). This list doesn’t include chains. 
             </p>
             <p className=" leading-relaxed mb-6">
                 I’m not a proficient coffee taster, so I’m not in pursuit of the perfectly made coffee (though I am always learning more about tasting coffee ;). My goal with this list is to seek out places that deliver amazing experiences and stories over coffee, have immaculate vibes, and meet some super cool people along the way. 
